d4b5467fc0c005ba6e5519ee4d0ab70d27c646e7
ivanternovyi <ivanternovyi@gmail.com>
2017-07-05 21:59:49 +0300

    Added template views for create/show/index actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,41 @@ class ItemsController < ApplicationController
     
     def index
         @items = Item.all
-        render text: @items.map {|i| "#{i.name}: #{i.price}"}.join("<br/>")
+        
+      # @items = Item.all
+      #render text: @items.map {|i| "#{i.name}: #{i.price}"}.join("<br/>")
     end
     
+    #/items/1 GET
+    def show
+        unless @item = Item.where(id: params[:id]).first 
+            render :text => "Page not found!", status: 404
+        end
+    end
+    
+    #/items/new GET
+    def new
+    end
+    
+    #/items/1/edit GET
+    def edit
+    end
+    
+    #/items POST
     def create
-        @item = Item.create(name: params[:name], description: params[:description], price: params[:price], real: params[:real], weight: params[:weight])
+       # @item = Item.create(name: params[:name], description: params[:description], price: params[:price], real: params[:real], weight: params[:weight])
         #Need item[variable] in the url zone
         #@item = Item.create(params[:item])
-        render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+       # render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+        render text: "Item created!"
     end
     
+    #/items/1 PUT
+    def update
+    end
+    
+    #/items/1 DELETE(POST)
+    def destroy
+    end
+        
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get ':controller(/:action(/:id))(.:format)'
+  resources :items
+    #  get ':controller(/:action(/:id))(.:format)'
 end
