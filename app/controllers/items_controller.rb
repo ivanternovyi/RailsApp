bc17aa00e0ae6aee3ffe8c80c13a96d6e6dd2e4a
ivanternovyi <ivanternovyi@gmail.com>
2017-07-10 16:57:20 +0300

    Added rating(created migration), added filters to ItemController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,22 @@
 class ItemsController < ApplicationController
     
+    before_filter :find_item,      only: [:show, :edit, :update, :destroy, :upvote]
+    before_filter :check_if_admin, only: [:edit, :update, :new, :create, :destroy]    
+    
     def index
         @items = Item.all
-        
-      # @items = Item.all
-      #render text: @items.map {|i| "#{i.name}: #{i.price}"}.join("<br/>")
     end
     
     #/items/1 GET
     def show
-        unless @item = Item.where(id: params[:id]).first 
+        unless @item 
             render :text => "Page not found!", status: 404
         end
+    end
+    
+    def expensive
+        @items = Item.where("price > 1000")
+        render "index"
     end
     
     #/items/new GET
@@ -22,7 +27,6 @@ class ItemsController < ApplicationController
     
     #/items/1/edit GET
     def edit
-        @item = Item.find(params[:id])
     end
     
     #/items POST
@@ -40,7 +44,6 @@ class ItemsController < ApplicationController
     
     #/items/1 PUT
     def update
-        @item = Item.find(params[:id])
         @parameters = params.require(:item).permit(:name, :price, :description, :weight)
         @item.update_attributes(@parameters)
         if @item.errors.empty?
@@ -52,9 +55,23 @@ class ItemsController < ApplicationController
     #GET - render
     #/items/1 DELETE(POST)
     def destroy
-        @item = Item.find(params[:id])
         @item.destroy
         redirect_to action: "index"
     end
         
+    def upvote
+        @item.increment!(:votes_count)
+        redirect_to action: :index
+    end
+    
+    private
+    
+    def find_item
+         @item = Item.where(id: params[:id]).first
+         render_404 unless @item
+    end
+ 
 end
+
+
+
