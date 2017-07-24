ab5a3d40c11404c61280cfd32195f5c46e4d285c
ivanternovyi <ivanternovyi@gmail.com>
2017-07-24 16:30:59 +0300

    Added 2 helpers to extract link and images from url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def urls_to_images(s)
+    s.gsub! /\s((https|http):\/\/.[\S]+\.(png|jpg|jpeg|svg|gif))\s/,
+     '<p><img src="\1" width="700" height="300"></p>'
+    s.html_safe
+  end
+
+  def urls_to_links(s)
+    s.gsub! /\s(https:\/\/.[\S]+)/, '<a href="\1">\1</a>'
+    s.html_safe
+  end
+
 end
