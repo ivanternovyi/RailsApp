202a75d724ca65238dc286d0892163a7975ccfed
ivanternovyi <ivanternovyi@gmail.com>
2017-08-10 22:57:04 +0300

    Redesigned item#show template

--- db/schema.rb
+++ db/schema.rb
@@ -70,15 +70,6 @@ ActiveRecord::Schema.define(version: 20170810093720) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "search_filters", id: false, force: :cascade do |t|
-    t.integer  "price_from"
-    t.integer  "price_to"
-    t.integer  "votes_from"
-    t.integer  "votes_to"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
